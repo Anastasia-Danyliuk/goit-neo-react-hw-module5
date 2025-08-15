768bc797c20e3619f1f66e5c5513a2e4dd39c9bd
bexots64 <bexots64@gmail.com>
2025-08-15 22:28:52 +0300

    Add React hw5

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -4,21 +4,35 @@ import MovieList from "../../components/MovieList/MovieList";
 import api from "../../api";
 import styles from "../../pages/MoviesPage/MoviesPage.module.css";
 
-
 export default function MoviesPage() {
     const [movies, setMovies] = useState([]);
     const [error, setError] = useState(null);
     const [searchParams, setSearchParams] = useSearchParams();
     const [searchInput, setSearchInput] = useState(searchParams.get("query") || "");
+    const [loading, setLoading] = useState(false);
+    const [noResults, setNoResults] = useState(false);
+
+    useEffect(() => {
+        const query = searchParams.get("query");
+        if (!query) return;
+
+        setLoading(true);
+        setError(null);
+        setNoResults(false);
+
+        api.get(`/search/movie?query=${query}&include_adult=false&language=en-US&page=1`)
+            .then(res => {
+                setMovies(res.data.results);
+                setNoResults(res.data.results.length === 0);
+            })
+            .catch(err => setError(err.message))
+            .finally(() => setLoading(false));
+    }, [searchParams]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (!searchInput) return;
-
-        setSearchParams({ query: searchInput }); // оновлюємо URL
-        api.get(`/search/movie?query=${searchInput}&include_adult=false&language=en-US&page=1`)
-            .then(res => setMovies(res.data.results))
-            .catch(err => setError(err.message));
+        if (!searchInput.trim()) return;
+        setSearchParams({ query: searchInput });
     };
 
     return (
@@ -34,8 +48,11 @@ export default function MoviesPage() {
                 <button type="submit">Search</button>
             </form>
 
-            {error && <p>{error}</p>}
-            <MovieList movies={movies} />
+            {loading && <p>Loading...</p>}
+            {error && <p style={{ color: "red" }}>Error: {error}</p>}
+            {noResults && <p>No results</p>}
+
+            {!loading && !error && !noResults && <MovieList movies={movies} />}
         </div>
     );
 }
